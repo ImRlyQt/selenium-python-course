4207b651ac86e237588f777c183a4899528d6fb3
ImRlyQt <ernestlesniak03@gmail.com>
2025-11-03 15:43:44 +0100

    add test marks

--- tests/test_math.py
+++ tests/test_math.py
@@ -1,10 +1,13 @@
+import pytest
+
+
 def add_two_numbers(a, b):
     return a + b
 
-
+@pytest.mark.math
 def test_small_numbers():
-    assert add_two_numbers(1, 2) == 30, "suma ma być 3"
+    assert add_two_numbers(1, 2) == 3, "suma ma być 3"
 
-
+@pytest.mark.math
 def test_large_numbers():
     assert add_two_numbers(100, 300) == 400, "suma ma być 400"
