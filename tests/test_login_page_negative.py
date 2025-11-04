051ef7ecaa9b7c40a647ef5e72371f738cbbf3a7
ImRlyQt <ernestlesniak03@gmail.com>
2025-11-04 15:23:48 +0100

    error messeges

--- tests/test_login_page_negative.py
+++ tests/test_login_page_negative.py
@@ -25,6 +25,10 @@ class TestNegativeScenarios:
         # 4. Push Submit button
         submit_button_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
         submit_button_locator.click()
-        time.sleep(5)
         # 5. Verify error message is displayed
+        error_message_locator = driver.find_element(By.ID, "error")
+        assert error_message_locator.is_displayed(), "Error is not displayed but should be"
+
         # 6. Verify error message text is Your username is invalid!
+        error_message = error_message_locator.text
+        assert error_message == "Your username is invalid!", "Error message is not expexted"
