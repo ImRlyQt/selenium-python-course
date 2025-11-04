648e67f0e3acbb6ded12ce56d96f03597e2dd165
ImRlyQt <ernestlesniak03@gmail.com>
2025-11-04 15:12:10 +0100

    create file test login page negative

---
+++ tests/test_login_page_negative.py
@@ -0,0 +1,21 @@
+# Test case 2: Negative username test
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class TestNegativeScenarios:
+
+    def test_negative_username(self):
+
+        # 1. Open page
+        driver = webdriver.Chrome()
+        driver.get("https://practicetestautomation.com/practice-test-login/")
+
+        # 2. Type username incorrectUser into Username field
+        username_locator = driver.find_element(By.ID, "username")
+        username_locator.send_keys("student")
+
+        # 3. Type password Password123 into Password field
+        # 4. Push Submit button
+        # 5. Verify error message is displayed
+        # 6. Verify error message text is Your username is invalid!
