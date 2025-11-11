d5bac6cd66f84f24dc45972c2624d1b3fc3b82e0
ImRlyQt <ernestlesniak03@gmail.com>
2025-11-11 23:06:21 +0100

    change driver func to fixture

--- tests/test_login_page_negative.py
+++ tests/test_login_page_negative.py
@@ -5,12 +5,15 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+
+@pytest.fixture()
 def driver():
     print("Creating chrome driver")
     my_driver = webdriver.Chrome()
     yield my_driver
     print("Closing chrome driver")
     my_driver.quit()
+
 
 class TestNegativeScenarios:
     @pytest.mark.login
