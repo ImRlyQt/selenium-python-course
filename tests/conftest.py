4d9670cdbe95580165bfb4209d3fae766679a20f
ImRlyQt <ernestlesniak03@gmail.com>
2025-11-17 12:53:06 +0100

    change engine from chrome to firefox

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,8 @@ import pytest
 @pytest.fixture()
 def driver():
     print("Creating chrome driver")
-    my_driver = webdriver.Chrome()
+    # my_driver = webdriver.Chrome() #no kurwa odpala chroma cnie
+    my_driver = webdriver.Firefox() #no kurwa odpala firefox co nie
     yield my_driver
     print("\nClosing chrome driver")
     my_driver.quit()
