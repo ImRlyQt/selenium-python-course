4847c8b244e63a499439b6b358c703f1d5ea620c
ImRlyQt <ernestlesniak03@gmail.com>
2025-11-19 13:43:01 +0100

    add debug mark and refactor code

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -20,11 +20,28 @@ class TestExceptions:
         row_2_input_element = wait.until(ec.visibility_of_element_located((By.XPATH, "//div[@id='row2']/input")))
         assert row_2_input_element.is_displayed(), "Row 2 input should be displayed but is not"
 
+    @pytest.mark.exceptions
+    @pytest.mark.debug
+    def test_element_not_interactable_exception(self, driver):
+        # 1. Open page
+        driver.get("https://practicetestautomation.com/practice-test-exceptions/")
+
+        # 2. Click Add button
+        add_button_locator = driver.find_element(By.ID, "add_btn")
+        add_button_locator.click()
+
+        # 3. Wait for the second row to load
+        wait = WebDriverWait(driver, 10)
+        row_2_input_element = wait.until(ec.visibility_of_element_located((By.XPATH, "//div[@id='row2']/input")))
+        assert row_2_input_element.is_displayed(), "Row 2 input should be displayed but is not"
+
         # 4. Type text into the second input field
-        row_2_text_locator = driver.find_element(By.XPATH, "//div[@id='rows']/div[3]/div[@class='row']/input[@type='text']")
+        row_2_text_locator = driver.find_element(By.XPATH,
+                                                 "//div[@id='rows']/div[3]/div[@class='row']/input[@type='text']")
         row_2_text_locator.send_keys("text")
         # 5. Push Save button using locator By.name(“Save”)
-        save_button_locator = driver.find_element(By.XPATH, "//div[@id='rows']/div[3]/div[@class='row']/button[@id='save_btn']")
+        save_button_locator = driver.find_element(By.XPATH,
+                                                  "//div[@id='rows']/div[3]/div[@class='row']/button[@id='save_btn']")
         save_button_locator.click()
         # 6. Verify text saved
         confirmation_element = driver.find_element(By.ID, "confirmation")
