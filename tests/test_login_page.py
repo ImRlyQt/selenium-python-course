4a399d00d48d1867eb9f3fe995e58157a797cc11
ImRlyQt <ernestlesniak03@gmail.com>
2025-11-24 12:00:51 +0100

    remove unused imports and refactor file test_login_page

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -1,5 +1,4 @@
 import pytest
-from selenium.webdriver.common.by import By
 
 from Practice.page_objects.logged_in_successfully import LoggedInSuccessfullyPage
 from Practice.page_objects.login_page import LoginPage
@@ -7,16 +6,13 @@ from Practice.page_objects.login_page import LoginPage
 
 class TestPositiveScenarios:
 
-
     @pytest.mark.login
     @pytest.mark.positive
     def test_positive_login(self, driver):
         login_page = LoginPage(driver)
         login_page.open()
-        login_page.execute_login("student","Password123")
+        login_page.execute_login("student", "Password123")
         login_in_page = LoggedInSuccessfullyPage(driver)
         assert login_in_page.expected_url == login_in_page.current_url, "Actual URL is not the same as expected"
         assert login_in_page.header == "Logged In Successfully", "Header is not expected"
         assert login_in_page.is_logout_button_displayed(), "Logout button should be visible"
-
-
