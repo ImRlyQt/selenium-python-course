1a82eb948655f0baa7645b8d6685061c73440e11
ImRlyQt <ernestlesniak03@gmail.com>
2025-11-25 11:46:53 +0100

    rewrite test_no_such_element_exception

--- page_objects/exceptions_page.py
+++ page_objects/exceptions_page.py
@@ -6,8 +6,8 @@ from Practice.page_objects.base_page import BasePage
 
 class ExceptionsPage(BasePage):
     __url = "https://practicetestautomation.com/practice-test-exceptions/"
-    __add_button = (By.ID, "add_btn")
-    __edit_button = (By.ID, "edit_btn")
+    __add_button_locator = (By.ID, "add_btn")
+    __edit_button_locator = (By.ID, "edit_btn")
     __row_1_save_button = (By.XPATH, "//div[@id='row1']/button[@name='Save']")
     __row_2_save_button = (By.XPATH, "//div[@id='row2']/button[@name='Save']")
     __row_1_input_element = (By.XPATH, "//div[@id='row1']/input")
@@ -17,5 +17,13 @@ class ExceptionsPage(BasePage):
     def __init__(self, driver: WebDriver):
         super().__init__(driver)
 
-    def open(self):
+    def _open(self):
         super()._open_url(self.__url)
+
+    def _add_second_row(self):
+        super()._click(self.__add_button_locator)
+        super()._wait_until_element_is_visible(self.__row_2_input_element)
+
+    def is_row_2_displayed(self) -> bool:
+        return super()._is_displayed(self.__row_2_input_element)
+
--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,25 +1,22 @@
 import time
+from logging import exception
+
 import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
+from Practice.page_objects.exceptions_page import ExceptionsPage
+
 
 class TestExceptions:
-
+    @pytest.mark.debug
     @pytest.mark.exceptions
     def test_no_such_element_exception(self, driver):
-        # 1. Open page
-        driver.get("https://practicetestautomation.com/practice-test-exceptions/")
-
-        # 2. Click Add button
-        add_button_locator = driver.find_element(By.ID, "add_btn")
-        add_button_locator.click()
-
-        # 3. Verify Row 2 input field is displayed
-        wait = WebDriverWait(driver, 10)
-        row_2_input_element = wait.until(ec.visibility_of_element_located((By.XPATH, "//div[@id='row2']/input")))
-        assert row_2_input_element.is_displayed(), "Row 2 input should be displayed but is not"
+        exceptions_page = ExceptionsPage(driver)
+        exceptions_page._open()
+        exceptions_page._add_second_row()
+        assert exceptions_page.is_row_2_displayed(), "Row 2 input should be displayed but is not"
 
     @pytest.mark.exceptions
     def test_element_not_interactable_exception(self, driver):
@@ -82,7 +79,6 @@ class TestExceptions:
             (By.ID, "instructions"))), "Instruction text element should not be displayed"
 
     @pytest.mark.exceptions
-    @pytest.mark.debug
     def test_timeout_exception(self, driver):
         # Open page
         driver.get("https://practicetestautomation.com/practice-test-exceptions/")
@@ -92,6 +88,7 @@ class TestExceptions:
 
         # Wait for 3 seconds for the second input field to be displayed
         wait = WebDriverWait(driver, 5.1)
-        row_2_input_element = wait.until(ec.visibility_of_element_located((By.XPATH, "//div[@id='row2']/input")), "Failed waiting for Row 2 input to be visible")
+        row_2_input_element = wait.until(ec.visibility_of_element_located((By.XPATH, "//div[@id='row2']/input")),
+                                         "Failed waiting for Row 2 input to be visible")
         # Verify second input field is displayed
         assert row_2_input_element.is_displayed()
