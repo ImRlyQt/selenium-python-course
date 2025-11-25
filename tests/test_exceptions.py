5771326fc25b258a606249e7e1943cff501cfaa4
ImRlyQt <ernestlesniak03@gmail.com>
2025-11-25 13:13:24 +0100

    all exeptions tests work rewrited

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -32,7 +32,6 @@ class TestExceptions:
         exceptions_page._row_1_input_text("żurek")
         assert exceptions_page._get_confirmation_message() == "Row 1 was saved", "Confirmation message is not expected"
 
-    @pytest.mark.debug
     @pytest.mark.exceptions
     def test_stale_element_reference_exception(self, driver):
         exceptions_page = ExceptionsPage(driver)
@@ -40,17 +39,11 @@ class TestExceptions:
         exceptions_page._add_second_row()
         assert not exceptions_page._are_instructions_displayed(), "Instruction text element should not be displayed"
 
+    @pytest.mark.debug
     @pytest.mark.exceptions
     def test_timeout_exception(self, driver):
-        # Open page
-        driver.get("https://practicetestautomation.com/practice-test-exceptions/")
+        exceptions_page = ExceptionsPage(driver)
+        exceptions_page._open()
+        exceptions_page._add_second_row()
+        assert exceptions_page._is_row_2_displayed(), "Row 2 input should be displayed but is not"
 
-        # Click Add button
-        driver.find_element(By.ID, "add_btn").click()
-
-        # Wait for 3 seconds for the second input field to be displayed
-        wait = WebDriverWait(driver, 5.1)
-        row_2_input_element = wait.until(ec.visibility_of_element_located((By.XPATH, "//div[@id='row2']/input")),
-                                         "Failed waiting for Row 2 input to be visible")
-        # Verify second input field is displayed
-        assert row_2_input_element.is_displayed()
